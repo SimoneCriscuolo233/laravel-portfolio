11d33d3778221f0286ce779a59703119dbfd94e2
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2026-02-15 17:46:57 +0100

    feat: Enhance project management views with edit and delete functionality

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,15 +2,18 @@
 @section("title", "Tutti i post")
 @section("content")
   <div class="container">
-    <a class="btn btn-primary" href="{{ route('project.create')}}">Aggiungi Progetto</a>
 
-    <table>
+
+    <table class="table">
       <thead>
-        <tr>
-          <th>Titolo</th>
-          <th>Autore</th>
-          <th>Categoria</th>
-        </tr>
+
+        <th>Titolo</th>
+        <th>Autore</th>
+        <th>Categoria</th>
+        <th>&nbsp;</th>
+        <th class="d-flex justify-content-end">
+          <a class="btn btn-primary" href="{{ route('project.create')}}">Aggiungi Progetto</a>
+        </th>
       </thead>
       <tbody>
 
@@ -19,7 +22,43 @@
             <td>{{$project->title}}</td>
             <td>{{$project->author}}</td>
             <td>{{$project->category}}</td>
-            <td><a href="{{ route("project.show", $project) }}">Visualizza</a></td>
+            <td><a href="{{ route("project.show", $project) }}" class="btn btn-primary">Visualizza</a></td>
+            <td>
+              <div class="d-flex p-2 gap-2">
+                <a class="btn btn-warning" href="{{ route('project.edit', $project) }}">Modifica</a>
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                  Elimina
+                </button>
+
+                <!-- Modal -->
+                <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                  aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Elimina il Progetto</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                        Vuoi eliminare definitivamente il progetto?
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                        <form action="{{ route('project.destroy', $project) }}" method="POST">
+                          @csrf
+                          @method("DELETE")
+                          <input type="submit" class="btn btn-danger" value="Elimina">
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+
+              </div>
+            </td>
           </tr>
         @endforeach
       </tbody>
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,50 +1,62 @@
 @extends("layouts.projects")
-@section("title", $project->title)
 @section("content")
+  <div class="container">
+    <div class="card">
+      <div class="card-header">
+        <h1>
+          {{ $project->title }}
+        </h1>
 
-  <div class="d-flex p-2 gap-2">
-    <a class="btn btn-warning" href="{{ route('project.edit', $project) }}">Modifica</a>
-    <!-- Button trigger modal -->
-    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-      Elimina
-    </button>
-
-    <!-- Modal -->
-    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-      aria-labelledby="staticBackdropLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5" id="staticBackdropLabel">Elimina il Progetto</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            Vuoi eliminare definitivamente il progetto?
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
-            <form action="{{ route('project.destroy', $project) }}" method="POST">
-              @csrf
-              @method("DELETE")
-              <input type="submit" class="btn btn-danger" value="Elimina">
-            </form>
-          </div>
-        </div>
       </div>
+      <div class="card-body">
+
+        <div class="d-flex p-2 gap-2 justify-content-between">
+          <h2 class="card-text">
+            - {{$project->author}}
+          </h2>
+          <div>
+            <a class="btn btn-warning" href="{{ route('project.edit', $project) }}">Modifica</a>
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+              Elimina
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+              aria-labelledby="staticBackdropLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Elimina il Progetto</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                    Vuoi eliminare definitivamente il progetto?
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                    <form action="{{ route('project.destroy', $project) }}" method="POST">
+                      @csrf
+                      @method("DELETE")
+                      <input type="submit" class="btn btn-danger" value="Elimina">
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <small class="card-text">
+          {{$project->category}}
+        </small>
+        <section class="card-text">
+          {{$project->content}}
+        </section>
+
+      </div>
+
     </div>
-
-
-
   </div>
-
-  <h2>
-    - {{$project->author}}
-  </h2>
-  <small>
-    {{$project->category}}
-  </small>
-  <section>
-    {{$project->content}}
-  </section>
-
 @endsection
